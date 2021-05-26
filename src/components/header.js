61d0d94aa0b5490f1c5784795ee670e3b60e6c96
st46593 <st46593@student.upce.cz>
2021-05-26 20:35:15 +0200

    changes for nadhlavou

--- src/components/header.js
+++ src/components/header.js
@@ -15,7 +15,7 @@ export default class Header extends React.Component {
         <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav" expand="lg"
                 collapseOnSelect={true}>
           <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}>Start Bootstrap</a>
+            <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={Scroller.handleAnchorScroll}>nadhlavou.cz</a>
             <Navbar.Toggle aria-controls="navbarResponsive"/>
             <Navbar.Collapse id="navbarResponsive">
               <Nav className="navbar-nav ml-auto my-2 my-lg-0">
@@ -23,16 +23,16 @@ export default class Header extends React.Component {
                            items={["about", "services", "portfolio", "contact"]}
                            currentClassName="active" rootEl={"#mainNav"} offset={-75}>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>About</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>O nás</Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Services</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#services" onClick={Scroller.handleAnchorScroll}>Nabízíme</Nav.Link>
                   </li>
                   <li className="nav-item">
                     <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Portfolio</Nav.Link>
                   </li>
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Contact</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Kontakt</Nav.Link>
                   </li>
                 </Scrollspy>
               </Nav>
@@ -43,14 +43,12 @@ export default class Header extends React.Component {
           <div className="container h-100">
             <div className="row h-100 align-items-center justify-content-center text-center">
               <div className="col-lg-10 align-self-end">
-                <h1 className="text-uppercase text-white font-weight-bold">Your Favorite Source of Free Bootstrap
-                  Themes</h1>
+                <h1 className="text-uppercase text-white font-weight-bold">nadhlavou.cz</h1>
                 <hr className="divider my-4"/>
               </div>
               <div className="col-lg-8 align-self-baseline">
-                <p className="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites
-                  using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
-                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Find Out More</a>
+                <p className="text-white-75 font-weight-light mb-5">Videa z ptačí perspektivy!</p>
+                <a className="btn btn-secondary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Zjistit více</a>
               </div>
             </div>
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import ModalVideo from "react-modal-video"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,47 +9,53 @@ import Scroller from "../components/scroller"
 import PortfolioModal from "../components/portfolio/modal"
 import PortfolioCarousel from "../components/portfolio/carousel"
 
+
 export default class IndexPage extends React.Component {
   constructor(props) {
     super(props)
     Scroller.handleAnchorScroll = Scroller.handleAnchorScroll.bind(this)
     this.state = {
       modalShow: false,
-      modalCurrent: 0
     }
-    this.handlePortfolioClick = this.handlePortfolioClick.bind(this);
-    this.setModal = this.setModal.bind(this);
+    this.handlePortfolioClick = this.handlePortfolioClick.bind(this)
+    this.setModal = this.setModal.bind(this)
   }
 
-  handlePortfolioClick(index, e) {
-    e.preventDefault();
-    this.setModal(true, index);
+  handlePortfolioClick(e) {
+    e.preventDefault()
+    this.setModal(true)
   }
 
-  setModal(isShown, current) {
+  setModal(isShown) {
     this.setState({
-      modalShow: isShown,
-      modalCurrent: current
-    });
+      modalShow: isShown
+    })
   }
 
   render() {
     return (
+      <>
       <Layout>
-        <SEO title="Home"/>
-        <section className="page-section bg-primary" id="about">
+      <SEO title="Home" />
+        <section className="page-section bg-secondary" id="about">
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
-                <h2 className="text-white mt-0">We've got what you need!</h2>
-                <hr className="divider light my-4"/>
-                <p className="text-white-50 mb-4">Start Bootstrap has everything you need to get your new website up
-                  and
-                  running in no time! Choose one of our open source, free to download, and easy to use themes! No
-                  strings
-                  attached!</p>
-                <a className="btn btn-light btn-xl js-scroll-trigger" href="#services"
-                   onClick={Scroller.handleAnchorScroll}>Get Started!</a>
+                <h2 className="text-white mt-0">
+                  Natočíme pro Vás video dle Vašich představ!
+                </h2>
+                <hr className="divider light my-4" />
+                <p className="text-white-50 mb-4">
+                  Vyberte si z našich služeb a my pro Vás zajistíme ten nejlepší
+                  výsledek!
+                </p>
+                <a
+                  className="btn btn-light btn-xl js-scroll-trigger"
+                  href="#services"
+                  onClick={Scroller.handleAnchorScroll}
+                >
+                  Nabízíme!
+                </a>
               </div>
             </div>
           </div>
@@ -56,35 +63,45 @@ export default class IndexPage extends React.Component {
 
         <section className="page-section" id="services">
           <div className="container">
-            <h2 className="text-center mt-0">At Your Service</h2>
-            <hr className="divider my-4"/>
+            <h2 className="text-center mt-0">Nabízíme</h2>
+            <hr className="divider my-4" />
             <div className="row">
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
-                  <i className="fas fa-4x fa-gem text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Sturdy Themes</h3>
-                  <p className="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+                  <i className="fas fa-4x fa-city text-secondary mb-4"></i>
+                  <h3 className="h4 mb-2">Prezentační videa</h3>
+                  <p className="text-muted mb-0">
+                    Videa měst, obcí, nebo turistických pamětihodností!
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
-                  <i className="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Up to Date</h3>
-                  <p className="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+                  <i className="fas fa-4x fa-home text-secondary mb-4"></i>
+                  <h3 className="h4 mb-2">Monitoring objektů</h3>
+                  <p className="text-muted mb-0">
+                    Monitoring a kontrola objektů, nemovitostí a těžko
+                    dostupných míst.
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
-                  <i className="fas fa-4x fa-globe text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Ready to Publish</h3>
-                  <p className="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+                  <i className="fas fa-4x fa-users text-secondary mb-4"></i>
+                  <h3 className="h4 mb-2">Společenské akce</h3>
+                  <p className="text-muted mb-0">
+                    Obecní slavnosti, slavnostní průvody, ale také svatby a
+                    narozeninové oslavy!
+                  </p>
                 </div>
               </div>
               <div className="col-lg-3 col-md-6 text-center">
                 <div className="mt-5">
-                  <i className="fas fa-4x fa-heart text-primary mb-4"></i>
-                  <h3 className="h4 mb-2">Made with Love</h3>
-                  <p className="text-muted mb-0">Is it really open source if it's not made with love?</p>
+                  <i className="fas fa-4x fa-video text-secondary mb-4"></i>
+                  <h3 className="h4 mb-2">Promo videa</h3>
+                  <p className="text-muted mb-0">
+                    Propagační videa společností, firem a podniků.
+                  </p>
                 </div>
               </div>
             </div>
@@ -95,129 +112,188 @@ export default class IndexPage extends React.Component {
           <div className="container-fluid p-0">
             <div className="row no-gutters">
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/1.jpg" onClick={this.handlePortfolioClick.bind(this, 0)}>
-                  <Img fluid={this.props.data.images.edges[0].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/1.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[0].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg" onClick={this.handlePortfolioClick.bind(this, 1)}>
-                  <Img fluid={this.props.data.images.edges[1].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/2.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[1].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg" onClick={this.handlePortfolioClick.bind(this, 2)}>
-                  <Img fluid={this.props.data.images.edges[2].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/3.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[2].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="images/portfolio/fullsize/4.jpg" onClick={this.handlePortfolioClick.bind(this, 3)}>
-                  <Img fluid={this.props.data.images.edges[3].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="images/portfolio/fullsize/4.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[3].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/5.jpg" onClick={this.handlePortfolioClick.bind(this, 4)}>
-                  <Img fluid={this.props.data.images.edges[4].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/5.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[4].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
               <div className="col-lg-4 col-sm-6">
-                <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg" onClick={this.handlePortfolioClick.bind(this, 5)}>
-                  <Img fluid={this.props.data.images.edges[5].node.childImageSharp.fluid}/>
+                <a
+                  className="portfolio-box"
+                  href="img/portfolio/fullsize/6.jpg"
+                  onClick={this.handlePortfolioClick.bind(this)}
+                >
+                  <Img
+                    fluid={
+                      this.props.data.images.edges[5].node.childImageSharp.fluid
+                    }
+                  />
                   <div className="portfolio-box-caption p-3">
                     <div className="project-category text-white-50">
                       Category
                     </div>
-                    <div className="project-name">
-                      Project Name
-                    </div>
+                    <div className="project-name">Project Name</div>
                   </div>
                 </a>
               </div>
             </div>
           </div>
-        </section>
-
-        <section className="page-section bg-dark text-white">
-          <div className="container text-center">
-            <h2 className="mb-4">Free Download at Start Bootstrap!</h2>
-            <a className="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Download Now!</a>
-          </div>
+          <ModalVideo channel='youtube' autoplay isOpen={this.state.modalShow} videoId="UxPkK8gW0hs" onClose={() => this.setModal(false)} />
         </section>
 
         <section className="page-section" id="contact">
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-8 text-center">
-                <h2 className="mt-0">Let's Get In Touch!</h2>
-                <hr className="divider my-4"/>
-                <p className="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an
-                  email
-                  and we will get back to you as soon as possible!</p>
+                <h2 className="mt-0">Kontaktujte nás!</h2>
+                <hr className="divider my-4" />
+                <p className="text-muted mb-5">
+                  Zaujala Vás naše nabídka? Neváhajte a zanechte nám zprávu,
+                  nebo nám zavolejte!
+                </p>
               </div>
             </div>
             <div className="row">
+              <form className="col-lg-12">
+                <div className="form-group row justify-content-center">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Emailová adresa"
+                  />
+                </div>
+                <div class="form-group row justify-content-center">
+                  <label for="exampleInputPassword1">Vaše zpráva</label>
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Vaše zpráva"
+                  />
+                </div>
+                <div class="row justify-content-center">
+                  <button type="submit" class="btn btn-secondary mb-5">
+                    Odeslat
+                  </button>
+                </div>
+              </form>
               <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                 <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
-                <div>+1 (202) 555-0149</div>
+                <div>+420 722 016 026</div>
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                <a className="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                <a className="d-block" href="mailto:info@nadhlavou.cz">
+                  info@nadhlavou.cz
+                </a>
               </div>
             </div>
           </div>
-        </section>
-        <PortfolioModal show={this.state.modalShow} onHide={() => this.setModal(false, 0)}>
-          <PortfolioCarousel images={this.props.data.images.edges} current={this.state.modalCurrent}/>
-        </PortfolioModal>
+        </section>      
       </Layout>
-    )
+      </>
+    ) 
   }
 }
 
-
 export const imageData = graphql`
   query {
-    images: allFile(filter: {relativePath: {glob: "portfolio/fullsize/*.jpg"}}, sort: {fields: name}) {
+    images: allFile(
+      filter: { relativePath: { glob: "portfolio/fullsize/*.jpg" } }
+      sort: { fields: name }
+    ) {
       edges {
         node {
           childImageSharp {
